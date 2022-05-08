940b6bfa700d266c94774ee298fe9e3848f0a3b1
Dominic.Blankenfeldt <d.blankenfeldt@mbd-team.de>
2022-05-08 17:16:17 +0200

    instal pwa

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  publicPath: "/bk24", //enter the name of the git repo here
+  outputDir: "./docs",
+  //pwa: {
+  //workboxPluginMode: "InjectManifest",
+  //workboxOptions: {
+  //swSrc: "src/service-worker.js",
+  //},
+  //},
+  css: {
+    extract: false,
+  },
+};
