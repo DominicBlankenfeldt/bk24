77b37112100396359fedfef1a71c55c3ae867e2e
Dominic.Blankenfeldt <d.blankenfeldt@mbd-team.de>
2022-05-08 17:17:14 +0200

    run biuld pwa

---
+++ docs/precache-manifest.3b5ad043994a9237a10c97e69488ad26.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1139e9b29e5b925717cefe00d6ff3dcd",
+    "url": "/bk24/img/background.jpg"
+  },
+  {
+    "revision": "45f11d36559a14d01b8d9b68f6e4808d",
+    "url": "/bk24/img/baustoffe.45f11d36.jpg"
+  },
+  {
+    "revision": "63044b975727067a18031532e603d971",
+    "url": "/bk24/img/fenster.63044b97.jpg"
+  },
+  {
+    "revision": "89e4defa4183a93fcf566dfed1ba8839",
+    "url": "/bk24/img/logo.89e4defa.png"
+  },
+  {
+    "revision": "31bd640f263098652595a22b08c963de",
+    "url": "/bk24/img/tür.31bd640f.jpg"
+  },
+  {
+    "revision": "f89719e86f862a3cc1816b96e4ddc19b",
+    "url": "/bk24/img/zubhör.f89719e8.jpg"
+  },
+  {
+    "revision": "a11eb52d9acf55d6f0733e98ea99e25b",
+    "url": "/bk24/index.html"
+  },
+  {
+    "revision": "198cb94b5bd4ca6f9699",
+    "url": "/bk24/js/app.3da13334.js"
+  },
+  {
+    "revision": "94aa8f23444f5b12aeb3",
+    "url": "/bk24/js/chunk-vendors.1d520758.js"
+  },
+  {
+    "revision": "26f0926cc9eae15794d2bc68ff959a04",
+    "url": "/bk24/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bk24/robots.txt"
+  }
+]);
